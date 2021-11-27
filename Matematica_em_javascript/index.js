df99d6a4e0f97bb4bab638b28dcd7dd643e2191c
Macmiller <macamagolf@gmail.com>
2021-11-27 02:58:05 -0300

    update file

---
+++ Matematica_em_javascript/index.js
@@ -0,0 +1,51 @@
+// Matemática básica
+
+let num1 = 100;
+
+console.log(10 + 25);
+console.log(num1 - 100);
+console.log(num1 * 25);
+console.log(num1 / 1500);
+
+
+// Operações Matemáticas Adicionais
+
+// Resto da divisão
+console.log(num1 % 1500);
+
+
+// Incrementa
+console.log(++num1);
+
+
+// Decrementa
+console.log(--num1);
+
+
+// Objeto Math
+
+// PI
+console.log(Math.PI);
+
+
+let num2 = 9;
+
+// Raiz quadrada
+console.log(Math.sqrt(num2));
+
+
+// Tangente
+console.log(Math.tan(num2));
+
+
+// Usando vários métodos
+console.log(Math.tan(Math.PI/180));
+
+
+// Retorna o valor de um número arredondado para o inteiro masi próximo
+console.log(Math.round(0.5));
+
+
+// Retorna o valor absoluto do número
+console.log(Math.abs('Teles'));
+
