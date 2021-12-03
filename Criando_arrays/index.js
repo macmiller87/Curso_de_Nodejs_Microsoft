d9d746a1bb78f4bb225b6a0cb587021cce190e5e
Macmiller <macamagolf@gmail.com>
2021-12-03 03:15:05 -0300

    update file

---
+++ Criando_arrays/index.js
@@ -0,0 +1,24 @@
+// Exemplo -> Crinado um array
+
+let animais = ['cachorro', 'gato', 'girafa', 'cavalo'];
+console.log(animais);
+
+let idiomas = new Array('Inglês', 'Português', 'Espanhol', 'Francês'); // Método alternativo para criar array
+let ingles = idiomas[0];
+console.log(ingles);
+
+
+// Exemplo medindo o tamnho de um array (length)
+
+let frutas = ['Banana', 'Pera', 'Laranja', 'Abacaxi'];
+console.log(frutas.length);
+
+
+// Exemplo -> Acessar o último elemento do array
+
+// let frutas = ['Banana', 'Pera', 'Laranja', 'Abacaxi'];
+// let abacaxi = frutas[frutas.length - 1];
+// console.log(abacaxi);
+
+
+
