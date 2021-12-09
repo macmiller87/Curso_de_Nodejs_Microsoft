54b53ba04ee0459f098050b698f4d68eaa025073
Macmiller <macamagolf@gmail.com>
2021-12-09 02:57:51 -0300

    update file

---
+++ Object_Notation_JSON/index.js
@@ -0,0 +1,20 @@
+// objeto chamado:  livro -> Book.
+
+const book = {
+
+    title: "Eloquent Javascript",
+    author: "Marijn Haverbeke",
+    isAvailable: true
+  
+};
+  
+
+// Json stringify
+  
+const bookToJson =  JSON.stringify(book); // Aqui está sendo setado na variavel o objeto acima, e usando o JSON.stringify() para transformar o objeto em json.
+console.log(bookToJson);
+
+const jsonObjt = '{"title":"Eloquent Javascript","author":"Marijn Haverbeke","isAvailable":true}';
+const books = JSON.parse(jsonObjt); // Aqui está sendo transformado para objeto novamente.
+console.log(books); // Aqui está sendo setado o objeto inteiro.
+console.log(books.title); // Aqui está sendo setado o objeto inteiro, e sendo imprimido no terminal
